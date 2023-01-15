e9f42e22af74f20e767e85580e552113c776d413
Sathvika-7284 <sathvika7276@gmail.com>
2023-01-15 23:23:40 +0800

    commit

--- taxi.py
+++ taxi.py
@@ -6,6 +6,7 @@ from prac_09.car import Car
 
 
 class Taxi(Car):
+
     """Specialised version of a Car that includes fare costs."""
 
     price_per_km = 1.23
--- taxi_test.py
+++ taxi_test.py
@@ -2,6 +2,7 @@ from prac_09.taxi import Taxi
 
 
 def main():
+
     """Test Taxi class."""
     my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
